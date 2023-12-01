4179adcf69d712ed606161fcb8a1328788849cc2
simonhng <simon.huang@tum.de>
2023-12-01 22:20:47 +0100

    add langchain

--- app/src/app/api/chat/route.ts
+++ app/src/app/api/chat/route.ts
@@ -1,21 +1,66 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
+import { NextRequest } from "next/server";
+import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
+
+import { ChatOpenAI } from "langchain/chat_models/openai";
+import { BytesOutputParser } from "langchain/schema/output_parser";
+import { PromptTemplate } from "langchain/prompts";
+import { env } from "@/env.mjs";
 import { openai } from "@/lib/openai";
 
-// IMPORTANT! Set the runtime to edge
 export const runtime = "edge";
 
-export async function POST(req: Request) {
-  // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+/**
+ * Basic memory formatter that stringifies and passes
+ * message history directly into the model.
+ */
+const formatMessage = (message: VercelChatMessage) => {
+  return `${message.role}: ${message.content}`;
+};
 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.chat.completions.create({
-    model: "gpt-3.5-turbo",
-    stream: true,
-    messages,
+const TEMPLATE = `You are a pirate named Patchy. All responses must be extremely verbose and in pirate dialect.
+
+Current conversation:
+{chat_history}
+
+User: {input}
+AI:`;
+
+/*
+ * This handler initializes and calls a simple chain with a prompt,
+ * chat model, and output parser. See the docs for more information:
+ *
+ * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
+ */
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const messages = body.messages ?? [];
+  const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
+  const currentMessageContent = messages[messages.length - 1].content;
+
+  const prompt = PromptTemplate.fromTemplate(TEMPLATE);
+  /**
+   * See a full list of supported models at:
+   * https://js.langchain.com/docs/modules/model_io/models/
+   */
+  const model = openai;
+  /**
+   * Chat models stream message chunks rather than bytes, so this
+   * output parser handles serialization and encoding.
+   */
+  const outputParser = new BytesOutputParser();
+
+  /*
+   * Can also initialize as:
+   *
+   * import { RunnableSequence } from "langchain/schema/runnable";
+   * const chain = RunnableSequence.from([prompt, model, outputParser]);
+   */
+  const chain = prompt.pipe(model).pipe(outputParser);
+
+  const stream = await chain.stream({
+    chat_history: formattedPreviousMessages.join("\n"),
+    input: currentMessageContent,
   });
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
-  // Respond with the stream
+
   return new StreamingTextResponse(stream);
 }
--- app/src/lib/openai.ts
+++ app/src/lib/openai.ts
@@ -1,13 +1,10 @@
 import { env } from "@/env.mjs";
-import OpenAI from "openai";
+import { ChatOpenAI } from "langchain/chat_models/openai";
 
-const resource = env.AZURE_OPENAI_RESOURCE;
-const model = env.AZURE_OPENAI_MODEL;
-const apiKey = env.AZURE_OPENAI_API_KEY;
-
-export const openai = new OpenAI({
-  apiKey,
-  baseURL: `https://${resource}.openai.azure.com/openai/deployments/${model}`,
-  defaultQuery: { "api-version": "2023-06-01-preview" },
-  defaultHeaders: { "api-key": apiKey },
+export const openai = new ChatOpenAI({
+  temperature: 0.9,
+  azureOpenAIApiKey: env.AZURE_OPENAI_API_KEY,
+  azureOpenAIApiVersion: "2023-06-01-preview",
+  azureOpenAIApiInstanceName: env.AZURE_OPENAI_RESOURCE,
+  azureOpenAIApiDeploymentName: env.AZURE_OPENAI_MODEL,
 });
