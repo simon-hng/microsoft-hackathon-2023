a7b712f39a9f3080e9c65d99d861abfac362e6cf
simonhng <simon.huang@tum.de>
2023-12-02 16:45:50 +0100

    fix env variables for deployment

--- app/src/env.mjs
+++ app/src/env.mjs
@@ -9,4 +9,10 @@ export const env = createEnv({
     RESEND_API_KEY: z.string().min(1),
   },
   client: {},
+  runtimeEnv: {
+    AZURE_OPENAI_API_KEY: process.env.AZURE_OPENAI_API_KEY,
+    AZURE_OPENAI_RESOURCE: process.env.AZURE_OPENAI_RESOURCE,
+    AZURE_OPENAI_MODEL: process.env.AZURE_OPENAI_MODEL,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
 });
