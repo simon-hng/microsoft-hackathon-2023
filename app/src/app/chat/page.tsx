a7b03395d41edc843724256a848b8416ef5d58e5
simonhng <simon.huang@tum.de>
2023-12-02 14:23:33 +0100

    refactor header into component

--- app/src/app/chat/page.tsx
+++ app/src/app/chat/page.tsx
@@ -20,20 +20,6 @@ export default function Chat() {
 
   return (
     <>
-      <header className="fixed w-full bg-primary text-primary-foreground px-8 py-4 z-10">
-        <svg
-          width="80"
-          height="42"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 73 38"
-        >
-          <path
-            d="M28 0v31h8V0h37v38h-7V7h-8v31h-7V7h-8v31H21V7h-7v31H7V7H0V0h28z"
-            fill="currentColor"
-          />
-        </svg>
-      </header>
-
       <main className="bg-gray-100 dark:bg-gray-900">
         <div className="min-h-screen flex flex-col p-4 py-28">
           {messages.length ? (
--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
+import { Header } from "@/components/header";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,7 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={GeistSans.className}>{children}</body>
+      <body className={GeistSans.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
