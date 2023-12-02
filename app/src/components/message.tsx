c8c5416d5d20c2a96267fbad69cfc4b3895186de
simonhng <simon.huang@tum.de>
2023-12-02 19:02:11 +0100

    refactor source list

--- app/src/components/message.tsx
+++ app/src/components/message.tsx
@@ -1,18 +1,34 @@
 import { Avatar } from "@/components/ui/avatar";
-import { BookOpen, Robot, User } from "@phosphor-icons/react";
+import { Robot, User } from "@phosphor-icons/react";
 import { Message } from "ai";
 import { MemoizedReactMarkdown } from "./markdown";
 import { CodeBlock } from "@/components/ui/codeblock";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
-import Link from "next/link";
-import {
-  HoverCard,
-  HoverCardContent,
-  HoverCardTrigger,
-} from "@radix-ui/react-hover-card";
+import { SourceList } from "./source-list";
 
-const MessageContent = ({ message }: MessageBubbleProps) => (
+interface ChatMessageProps {
+  message: Message;
+  showSources?: boolean;
+}
+
+export const ChatMessage = ({ message, showSources }: ChatMessageProps) => {
+  return (
+    <>
+      {message.role === "user" ? (
+        <UserMessage key={message.id} message={message} />
+      ) : (
+        <BotMessage
+          key={message.id}
+          message={message}
+          showSources={showSources}
+        />
+      )}
+    </>
+  );
+};
+
+const MessageContent = ({ message }: ChatMessageProps) => (
   <MemoizedReactMarkdown
     className="prose break-words dark:prose-invert prose-p:leading-relaxed prose-pre:p-0"
     remarkPlugins={[remarkGfm, remarkMath]}
@@ -53,52 +69,8 @@ const MessageContent = ({ message }: MessageBubbleProps) => (
     {message.content}
   </MemoizedReactMarkdown>
 );
-interface MessageBubbleProps {
-  message: Message;
-  showSources?: boolean;
-}
 
-const SourceList = () => {
-  return (
-    <div className="flex items-center gap-2 text-gray-700 dark:text-dark-300 text-sm mt-3">
-      <BookOpen className="w-6 h-6" />
-      <p>
-        Sources:{" "}
-        <HoverCard>
-          <HoverCardTrigger asChild>
-            <Link href="" className="underline underline-offset-2">
-              TUM.de
-            </Link>
-          </HoverCardTrigger>
-          <HoverCardContent className="w-80 data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade bg-white p-5 data-[state=open]:transition-all">
-            <div className="flex justify-between space-x-4">
-              <div className="space-y-2">
-                <h4 className="text-sm font-semibold">
-                  TUM. The Entrepreneurial - Technical University of Munich
-                </h4>
-                <p className="text-sm">
-                  is a leading university for natural sciences and engineering
-                  in Germany and the world. Learn about its innovation, talent,
-                  excellence and responsibility, and discover its news, research
-                  results and upcoming events.
-                </p>
-                <Link
-                  className="flex text-blue-500 underline"
-                  target="_blank"
-                  href="http://tum.de"
-                >
-                  Visit Source
-                </Link>
-              </div>
-            </div>
-          </HoverCardContent>
-        </HoverCard>
-      </p>
-    </div>
-  );
-};
-
-const BotMessage = ({ message, showSources }: MessageBubbleProps) => {
+const BotMessage = ({ message, showSources }: ChatMessageProps) => {
   return (
     <div className="flex items-end space-x-2">
       <Avatar className="bg-primary text-primary-foreground items-center justify-center">
@@ -114,7 +86,7 @@ const BotMessage = ({ message, showSources }: MessageBubbleProps) => {
   );
 };
 
-const UserMessage = ({ message }: MessageBubbleProps) => {
+const UserMessage = ({ message }: ChatMessageProps) => {
   return (
     <div className="flex-1 overflow-y-auto space-y-4">
       <div className="flex items-end justify-end space-x-2">
@@ -128,21 +100,5 @@ const UserMessage = ({ message }: MessageBubbleProps) => {
         </Avatar>
       </div>
     </div>
-  );
-};
-
-export const ChatMessage = ({ message, showSources }: MessageBubbleProps) => {
-  return (
-    <>
-      {message.role === "user" ? (
-        <UserMessage key={message.id} message={message} />
-      ) : (
-        <BotMessage
-          key={message.id}
-          message={message}
-          showSources={showSources}
-        />
-      )}
-    </>
   );
 };
