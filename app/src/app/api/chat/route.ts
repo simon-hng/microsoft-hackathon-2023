841438cf451750ff9ce8917e25df514404d0b6c7
simonhng <simon.huang@tum.de>
2023-12-02 13:10:28 +0100

    refactor langhchain to lib folder

--- app/src/app/api/chat/route.ts
+++ app/src/app/api/chat/route.ts
@@ -1,32 +1,14 @@
 import { NextRequest } from "next/server";
 import { Message as VercelChatMessage, StreamingTextResponse } from "ai";
-
-import { BytesOutputParser } from "langchain/schema/output_parser";
-import { PromptTemplate } from "langchain/prompts";
-import { openai } from "@/lib/openai";
+import { retrievalChain } from "@/lib/langchain/simple-retrieval-chain";
 
 export const runtime = "edge";
 
-/**
- * Basic memory formatter that stringifies and passes
- * message history directly into the model.
- */
 const formatMessage = (message: VercelChatMessage) => {
   return `${message.role}: ${message.content}`;
 };
 
-const TEMPLATE = `You are a helpful support bot that helps students at TUM
-
-Current conversation:
-{chat_history}
-
-User: {input}
-AI:`;
-
 /*
- * This handler initializes and calls a simple chain with a prompt,
- * chat model, and output parser. See the docs for more information:
- *
  * https://js.langchain.com/docs/guides/expression_language/cookbook#prompttemplate--llm--outputparser
  */
 export async function POST(req: NextRequest) {
@@ -35,27 +17,7 @@ export async function POST(req: NextRequest) {
   const formattedPreviousMessages = messages.slice(0, -1).map(formatMessage);
   const currentMessageContent = messages[messages.length - 1].content;
 
-  const prompt = PromptTemplate.fromTemplate(TEMPLATE);
-  /**
-   * See a full list of supported models at:
-   * https://js.langchain.com/docs/modules/model_io/models/
-   */
-  const model = openai;
-  /**
-   * Chat models stream message chunks rather than bytes, so this
-   * output parser handles serialization and encoding.
-   */
-  const outputParser = new BytesOutputParser();
-
-  /*
-   * Can also initialize as:
-   *
-   * import { RunnableSequence } from "langchain/schema/runnable";
-   * const chain = RunnableSequence.from([prompt, model, outputParser]);
-   */
-  const chain = prompt.pipe(model).pipe(outputParser);
-
-  const stream = await chain.stream({
+  const stream = await retrievalChain.stream({
     chat_history: formattedPreviousMessages.join("\n"),
     input: currentMessageContent,
   });
