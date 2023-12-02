b305b95e6fe670efd3a461b991d20c3cb0cc8546
simonhng <simon.huang@tum.de>
2023-12-02 17:04:30 +0100

    remove unused completions

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,28 +1,7 @@
-"use client";
-
-import { useCompletion } from "ai/react";
-
-export default function SloganGenerator() {
-  const { completion, input, handleInputChange, handleSubmit } =
-    useCompletion();
-
+export default function Home() {
   return (
     <div className="mx-auto w-full max-w-md py-24 flex flex-col stretch">
-      <form onSubmit={handleSubmit}>
-        <input
-          className="fixed w-full max-w-md bottom-0 border border-gray-300 rounded mb-8 shadow-xl p-2 dark:text-black"
-          value={input}
-          placeholder="Describe your business..."
-          onChange={handleInputChange}
-        />
-      </form>
-      {completion ? (
-        <div className="whitespace-pre-wrap my-4">{completion}</div>
-      ) : (
-        <div>
-          Enter a business description and click enter to generate slogans.
-        </div>
-      )}
+      <h1>Welcome to you TUM AI Assistant 🤖</h1>
     </div>
   );
 }
