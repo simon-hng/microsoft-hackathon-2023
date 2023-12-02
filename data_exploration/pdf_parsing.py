610546bd3335ee7aaf1cecf82bfa628644fd8851
Kaktus500 <delseith.paul@gmail.com>
2023-12-02 17:55:59 +0100

    Fixed a couple of bugs like line break detection, alphabetical index appended, Courses category broken

--- data_exploration/pdf_parsing.py
+++ data_exploration/pdf_parsing.py
@@ -1,35 +1,70 @@
-import pdfminer.high_level
-import re
+from langchain.document_loaders import PDFMinerPDFasHTMLLoader
+from bs4 import BeautifulSoup
 import json
-pdf_path = "/Users/pauldelseith/Documents/microsoft-hackathon-2023/Data/Modulhandbuecher/16_167_2021_Modulehandbook_Master's Program Finance and Information Management__20221118.pdf"
-text = pdfminer.high_level.extract_text(pdf_path)
-pages = pdfminer.high_level.extract_pages(pdf_path)
-page_nr = sum(1 for _ in pages)
-pdf_text_split = text.split("Module Description")
-pattern = r'\n\n(.*?)\n\n'
-pdf_text_dict = {}
-for text_chunk in pdf_text_split[1:-1]:
-    match = re.search(pattern, text_chunk, re.DOTALL)
-    if match:
-        pdf_text_dict[match.group(1)] = text_chunk.replace(match.group(1), "")
-    else:
-        print("Test")
-with open("module_descriptions_dict_2.json", 'w') as json_file:
-    json.dump(pdf_text_dict, json_file)
+from pathlib import Path
 categories = ['Module Level:', 'Language:', 'Duration:', 
               'Frequency:', 'Credits:', 'Total Hours:', 
               'Self-study Hours:', 'Contact Hours:', 'Description of Examination Method:',
               'Repeat Examination:', '(Recommended) Prerequisites:', 'Content:', 'Intended Learning Outcomes:', 
               'Teaching and Learning Methods:', 'Media:', 'Reading List:', 'Responsible for Module:', 
               'Courses (Type of course, Weekly hours per semester), Instructor:']
-categorized = {}
-key = 'WI001287: Basics of FIM | Basics of FIM'
-value = pdf_text_dict[key]
-chopped_rest = value
-temp = chopped_rest.split(category)
-chopped_rest = temp[1]
-for category in categories:
-    temp = chopped_rest.split(category)
-    categorized[category] = temp[0]
-    if len(temp) > 1:
-        chopped_rest = temp[1]
+pdf_path = pdf_path = Path("/Users/pauldelseith/Documents/microsoft-hackathon-2023/Data/Modulhandbuecher/16_167_2021_Modulehandbook_Master's Program Finance and Information Management__20221118.pdf")
+loader = PDFMinerPDFasHTMLLoader(str(pdf_path))
+data = loader.load()[0]   # entire PDF is loaded as a single Document
+page_count = 68
+
+# extract the content of the page by getting the outer divs
+soup = BeautifulSoup(data.page_content,'html.parser')
+content = soup.find_all('div')
+
+# filter out Alphabetical Index
+for idx, element in enumerate(content):
+    if 'Alphabetical Index' in str(element):
+        content = content[:idx]
+        break
+
+# extract the module descriptions
+module_descriptions = {}
+current_module_name = ""
+for idx, element in enumerate(content):
+    if 'Module Description' in str(element):
+        module_name = content[idx+1].text
+        module_name = module_name.replace('\n', '')
+        module_descriptions[module_name] = []
+        current_module_name = module_name
+    elif current_module_name != "" and element.text.replace('\n', '') != current_module_name:
+        module_descriptions[current_module_name].append(element)
+
+# extract the categories in every module description
+module_descr_classified = {}
+page_break_detected = False
+page_break_detected_idx = 0
+for key, value in module_descriptions.items():
+    module_descr_classified[key] = {}
+    extr_categorie = ""
+    for idx, val in enumerate(value):
+        if any(categorie in val.text for categorie in categories):
+            txt_split = val.text.split(":")  
+            extr_categorie = txt_split[0]
+            if extr_categorie == "Credits":
+                # remove weird "*" for Credits
+                extr_content = txt_split[1].replace("*","")
+            else:
+                extr_content = ''.join(txt_split[1:])
+            module_descr_classified[key][extr_categorie] = extr_content.replace('\n', '')
+            # check if page break is detected
+            if idx + 2 < len(value) and f" of {page_count}" in value[idx+2].text:
+                page_break_detected = True
+                page_break_detected_idx = idx
+            else:
+                page_break_detected = False
+        elif idx == page_break_detected_idx + 4 and page_break_detected:
+            # add text after page break to the last category
+            module_descr_classified[key][extr_categorie] = ''.join([module_descr_classified[key][extr_categorie]," ", val.text.replace('\n', '')])
+        elif idx < (len(value) - 4) and extr_categorie == "Courses (Type of course, Weekly hours per semester), Instructor":
+            # add text after Courses to the last category since there are weird breaks here
+            module_descr_classified[key][extr_categorie] = ''.join([module_descr_classified[key][extr_categorie],", ", val.text.replace('\n', '')])
+    
+
+with open(f"{pdf_path.stem}.json", 'w', encoding="utf-8") as json_file:
+    json.dump(module_descr_classified, json_file, ensure_ascii=False)
