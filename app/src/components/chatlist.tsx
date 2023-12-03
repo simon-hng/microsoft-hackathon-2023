1a8aa8b6c44cf49f39869a1a33d4e85c136f11c3
simonhng <simon.huang@tum.de>
2023-12-03 03:39:35 +0100

    add loading spinner for chat

--- app/src/components/chatlist.tsx
+++ app/src/components/chatlist.tsx
@@ -1,5 +1,6 @@
 import { type Message } from "ai";
 import { ChatMessage } from "./message";
+import { CircleNotch } from "@phosphor-icons/react";
 
 export interface ChatList {
   messages: Message[];
@@ -16,6 +17,11 @@ export function ChatList({ messages, isLoading }: ChatList) {
       {messages.map((message, index) => (
         <ChatMessage key={index} message={message} showSources={!isLoading} />
       ))}
+      {isLoading && (
+        <div className="flex justify-center mt-4">
+          <CircleNotch className="animate-spin w-10 h-10 text-primary"/>
+        </div>
+      )}
     </div>
   );
 }
