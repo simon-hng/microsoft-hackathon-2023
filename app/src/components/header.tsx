b157894134e58dbed5d7429f633484aed8bbc17e
simonhng <simon.huang@tum.de>
2023-12-03 11:28:11 +0100

    add navigation

--- app/src/components/header.tsx
+++ app/src/components/header.tsx
@@ -1,17 +1,21 @@
+import Link from "next/link";
+
 export const Header = () => {
   return (
     <header className="fixed z-10 w-full bg-primary px-8 py-4 text-primary-foreground">
-      <svg
-        width="80"
-        height="42"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 73 38"
-      >
-        <path
-          d="M28 0v31h8V0h37v38h-7V7h-8v31h-7V7h-8v31H21V7h-7v31H7V7H0V0h28z"
-          fill="currentColor"
-        />
-      </svg>
+      <Link href="/">
+        <svg
+          width="80"
+          height="42"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 73 38"
+        >
+          <path
+            d="M28 0v31h8V0h37v38h-7V7h-8v31h-7V7h-8v31H21V7h-7v31H7V7H0V0h28z"
+            fill="currentColor"
+          />
+        </svg>
+      </Link>
     </header>
   );
 };
